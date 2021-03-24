1582a408f4b58bcb1eef9ac26c9d0da9344c213e
amj311 <amjudd315@gmail.com>
2021-03-24 05:55:51 -0600

    moved api to glitch.com

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ const app = new Vue({
     loading: true,
     showOpenModal: false,
     allResumes: null,
-    apiUrl: "http://localhost:4200/api",
+    apiUrl: "https://functional-kindly-strand.glitch.me/api/",
     resume: null,
     currentSaveQueue: null,
     allowSave: true,
