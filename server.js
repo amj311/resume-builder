7f8feb47129d75f89d77557a95b1cdbb4a9eabb5
amj311 <amjudd315@gmail.com>
2021-03-24 09:40:29 -0600

    imported cmd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cmd = require("node-cmd");
 
 const app = express();
 app.use(bodyParser.json());
